08d750ac20dfd085a837bff2e4cf3b32d29af80e
Rikki Rabinowitz <rikkirabz@gmail.com>
2016-08-13 21:59:20 -0400

    first commit

---
+++ src/components/Globe.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import util from '../util/helper.js';
+import CreateDisplay from './CreateDisplay.js';
+import '../css/Home.css';
+
+class Globe extends Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      response: [],
+      geoResponse: []
+    }
+  };
+
+  handleAllClick(event){
+    event.preventDefault();
+    util.getAll()
+    .then((json) => {
+    this.setState({
+      response: json
+    })
+    this.calculateDistance();
+    })
+  }
+
+  calculateDistance(){
+    var geoArray = [];
+
+    const results = this.state.response;
+    results.map((res)=>{
+      if(res.location){
+        let long = res.location.coordinates[0];
+        let lat = res.location.coordinates[1];
+        const distance = this.haversine(this.props.globe, lat, long);
+        if(distance < 10){
+          geoArray.push(res)
+        }
+      }
+    })
+
+    this.setState({
+      geoResponse: geoArray
+    })
+  }
+
+  haversine(globe, lat, long){
+    const currentLat = globe.latitude;
+    const currentLong = globe.longitude;
+    const APILat = lat;
+    const APILong = long;
+    const radius = 3959;
+    const distanceLat = this.calculateDegreesToRadius(Math.abs(APILat - currentLat));
+    const distanceLong = this.calculateDegreesToRadius(Math.abs(APILong - currentLong));
+
+    const a = Math.sin(distanceLat/2) * Math.sin(distanceLat/2) +
+              Math.cos(this.calculateDegreesToRadius(APILat)) * Math.cos(this.calculateDegreesToRadius(currentLat)) *
+              Math.sin(distanceLong/2) * Math.sin(distanceLong/2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    const d = radius * c;
+    return d;
+  }
+
+  calculateDegreesToRadius(num){
+    return num * (Math.PI/180);
+  }
+
+  render(){
+    return(
+      <div className="home">
+        <button className="globe" onClick={this.handleAllClick.bind(this)}>Find a park near me!</button>
+        <CreateDisplay geo={this.state.geoResponse}/>
+      </div>
+    )
+  }
+}
+
+export default Globe
